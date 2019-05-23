7248482e0d26f8e3171a4d1b96c4d4fe0d3c0a02
Ruslan Mustaev <mustaev.ruslan@hotmail.com>
2019-05-23 21:30:04 +0300

    Добавил LocalDateConverter

--- download/src/main/java/com/projectlucker/downloader/converters/LocalDateConverter.java
+++ download/src/main/java/com/projectlucker/downloader/converters/LocalDateConverter.java
@@ -2,6 +2,7 @@ package com.projectlucker.downloader.converters;
 
 import org.springframework.boot.context.properties.ConfigurationPropertiesBinding;
 import org.springframework.core.convert.converter.Converter;
+import org.springframework.lang.Nullable;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDate;
@@ -10,11 +11,12 @@ import java.time.format.DateTimeFormatter;
 @Component
 @ConfigurationPropertiesBinding
 public class LocalDateConverter implements Converter<String, LocalDate> {
-  @Override
-  public LocalDate convert(String source) {
-      if(source==null){
-          return null;
-      }
-      return LocalDate.parse(source, DateTimeFormatter.ofPattern("dd.MM.uuuu"));
-  }
+
+    @Override
+    public LocalDate convert(@Nullable String source) {
+        if (source == null) {
+            return null;
+        }
+        return LocalDate.parse(source, DateTimeFormatter.ofPattern("dd.MM.uuuu"));
+    }
 }
