776f4a35dff0c33e93e9853d0d1fbc8e45c21434
Ruslan Mustaev <mustaev.ruslan@hotmail.com>
2019-05-24 19:29:13 +0300

    Добавилд параметры для Финама

--- download/src/main/java/com/projectlucker/downloader/batch/FinamSourceDownloader.java
+++ download/src/main/java/com/projectlucker/downloader/batch/FinamSourceDownloader.java
@@ -13,10 +13,11 @@ import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
 
-import java.util.Arrays;
-import java.util.Collections;
+import java.time.format.DateTimeFormatter;
+import java.util.HashMap;
+import java.util.Map;
 
-import static java.util.Collections.*;
+import static java.util.Collections.singletonList;
 
 @Component
 @RequiredArgsConstructor
@@ -33,29 +34,31 @@ public class FinamSourceDownloader implements SourceDownloader {
     @Override
     public DownloadData download(Chart chart) {
 
+        FinamQueryParams params = new FinamQueryParams(chart);
+
         HttpHeaders headers = new HttpHeaders();
         headers.set("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:66.0) Gecko/20100101 Firefox/66.0");
         headers.setAccept(singletonList(MediaType.APPLICATION_OCTET_STREAM));
 
         UriComponentsBuilder uriBuilder = UriComponentsBuilder
                 .fromHttpUrl("http://export.finam.ru")
-                .path("SPFB.SBRF-6.19_190416_190417.csv")
+                .path(params.getPath())
                 .queryParam("market", 14)
-                .queryParam("em", 495895)
-                .queryParam("code", "SPFB.SBRF - 6.19")
+                .queryParam("em", params.getEm())
+                .queryParam("code", params.getCode())
                 .queryParam("apply", 0)
-                .queryParam("df", 16)
-                .queryParam("mf", 3)
-                .queryParam("yf", 2019)
-                .queryParam("from", "16.04.2019")
-                .queryParam("dt", 17)
-                .queryParam("mt", 3)
-                .queryParam("yt", 2019)
-                .queryParam("to", "17.04.2019")
-                .queryParam("p", 2)
-                .queryParam("f", "SPFB.SBRF - 6.19_190416_190417")
+                .queryParam("df1", params.getDf())
+                .queryParam("mf", params.getMf())
+                .queryParam("yf", params.getYf())
+                .queryParam("from", params.getFrom())
+                .queryParam("dt", params.getDt())
+                .queryParam("mt", params.getMt())
+                .queryParam("yt", params.getYt())
+                .queryParam("to", params.getTo())
+                .queryParam("p", params.getP())
+                .queryParam("f", params.getF())
                 .queryParam("e", ".csv")
-                .queryParam("cn", "SPFB.SBRF - 6.19")
+                .queryParam("cn", params.getCn())
                 .queryParam("dtf", 1)
                 .queryParam("tmf", 1)
                 .queryParam("MSOR", 0)
@@ -75,5 +78,91 @@ public class FinamSourceDownloader implements SourceDownloader {
                 byte[].class);
         System.out.println(entity);
         return new DownloadData(chart, response.getBody());
+    }
+
+    private static class FinamQueryParams {
+
+        private static DateTimeFormatter df1 = DateTimeFormatter.ofPattern("uuMMdd");
+        private static DateTimeFormatter df2 = DateTimeFormatter.ofPattern("dd.MM.uuuu");
+        private static Map<String, Integer> emMap = new HashMap<>();
+        private static Map<String, Integer> timeframeMap = new HashMap<>();
+
+        static {
+            timeframeMap.put("1m", 2);
+            timeframeMap.put("5m", 3);
+            timeframeMap.put("10m", 4);
+            timeframeMap.put("15m", 5);
+            timeframeMap.put("30m", 6);
+            timeframeMap.put("1h", 7);
+            timeframeMap.put("1d", 8);
+            timeframeMap.put("1w", 9);
+            timeframeMap.put("1n", 10); // 1 месяц
+
+
+            emMap.put("SPFB.RTS-9.19", 478555);
+            emMap.put("SPFB.SBRF-6.19", 495895);
+        }
+
+        private final Chart chart;
+
+        FinamQueryParams(Chart chart) {
+            this.chart = chart;
+        }
+
+        int getP() {
+            return timeframeMap.get(chart.getTimeframe());
+        }
+
+        String getPath() {
+            return getF() + ".csv";
+        }
+
+        String getCode() {
+            return chart.getCode();
+        }
+
+        int getDf() {
+            return chart.getStartDate().getDayOfMonth();
+        }
+
+        int getMf() {
+            return chart.getStartDate().getMonthValue() - 1;
+        }
+
+        int getYf() {
+            return chart.getStartDate().getYear();
+        }
+
+        int getDt() {
+            return chart.getEndDate().getDayOfMonth();
+        }
+
+        int getMt() {
+            return chart.getEndDate().getMonthValue() - 1;
+        }
+
+        int getYt() {
+            return chart.getEndDate().getYear();
+        }
+
+        String getFrom() {
+            return chart.getStartDate().format(df2);
+        }
+
+        String getTo() {
+            return chart.getEndDate().format(df2);
+        }
+
+        String getF() {
+            return chart.getCode() + "_" + chart.getStartDate().format(df1) + chart.getStartDate().format(df1);
+        }
+
+        String getCn() {
+            return chart.getCode();
+        }
+
+        int getEm() {
+            return emMap.get(chart.getCode());
+        }
     }
 }
